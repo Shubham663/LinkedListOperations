39b7c7f5e0fc4f433be78aa34b713cc4d7b260d0
Shubham663 <mittalshubham05@gmail.com>
2020-10-07 12:45:13 +0530

    adding node at first

---
+++ src/main/java/bridgelabz/LinkedListOperations/MainClass.java
@@ -0,0 +1,18 @@
+package bridgelabz.LinkedListOperations;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainClass 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Linked List Operations!" );
+        LinkedList linkedList = new LinkedList();
+        linkedList.addNodeAtFirst(70);
+        linkedList.addNodeAtFirst(30);
+        linkedList.addNodeAtFirst(56);
+        linkedList.printList();
+    }
+}
